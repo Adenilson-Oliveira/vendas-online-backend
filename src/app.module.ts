075b7815cff6e0e6af7eebe616af06fdeafa5d7c
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2023-05-25 20:15:35 -0300

    added conection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'vendas',
+      host: 'localhost',
+      password: 'mysecretpassword',
+      port: 5432,
+      username: 'postgres',
+      synchronize: true,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return { ...createUser, password: undefined };
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUsers() {
+    return this.userService.getAllUser();
   }
 }
